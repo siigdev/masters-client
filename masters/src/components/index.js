c9237319e2e5981d22bd2f0deb282069aa84a27a
siigdev <sebbahlol@gmail.com>
2020-05-08 17:56:11 +0200

    Updated the folder structure pattern

---
+++ masters/src/components/index.js
@@ -0,0 +1,8 @@
+import Block from './Block';
+import Text from './Text';
+import Input from './Input';
+import Button from './Button';
+import Card from './Card';
+import Divider from './Divider';
+
+export { Block, Text, Input, Button, Card, Divider };
---
+++ masters/src/constants/index.js
@@ -0,0 +1,6 @@
+import * as theme from './Theme';
+import * as APIKeys from './APIKeys';
+
+export {
+    theme, APIKeys
+};
